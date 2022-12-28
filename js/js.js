26d8dbfbed8a448970e2d7609caf566d928d1189
Mallu0007 <117072658+Mallu0007@users.noreply.github.com>
2022-12-28 20:53:53 +0530

    Main c

---
+++ js/js.js
@@ -0,0 +1,102 @@
+// const text = document.querySelector (".sec-text");
+    
+
+//     const textLoad = () => {
+//         setTimeout(() => {
+//             text.textContent = "UX/UI Designer";
+//         }, 0);
+//         setTimeout(() => {
+//             text.textContent = "Front End Developer";
+//         }, 3000);
+//         setTimeout(() => {
+//             text.textContent = "WordPress Developer";
+//         }, 6000);
+//     }
+//     textLoad ();
+//     setInterval (textLoad, 9000);
+
+var typed = new Typed(".texting", {
+            strings: ["Front End Developer", "UX/UI Designer", "WordPress Developer", "Designer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+});
+    
+
+
+
+
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         // sticky navbar on scroll script
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+        
+//         // scroll-up button show/hide script
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
+
+//     // slide-up script
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         // removing smooth scroll on slide-up button click
+//         $('html').css("scrollBehavior", "auto");
+//     });
+
+//     $('.navbar .menu li a').click(function(){
+//         // applying again smooth scroll on menu items click
+//         $('html').css("scrollBehavior", "smooth");
+//     });
+
+//     // toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+
+//     // typing text animation script
+//     var typed = new Typed(".typing", {
+//         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     var typed = new Typed(".typing-2", {
+//         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     // owl carousel script
+//     $('.carousel').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         autoplay: true,
+//         autoplayTimeOut: 2000,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0:{
+//                 items: 1,
+//                 nav: false
+//             },
+//             600:{
+//                 items: 2,
+//                 nav: false
+//             },
+//             1000:{
+//                 items: 3,
+//                 nav: false
+//             }
+//         }
+//     });
+// });
